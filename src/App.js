b2a88af1ce233818fb88a7ffbf5e97abc942c68e
daikou1928 <daikou1928@gmail.com>
2024-07-11 10:34:57 +0900

    add textbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React,{useState} from "react";
+
 function App() {
+  const [text, setText] = useState("");
+  const [addText, setAddText] = useState("");
+
+  const onClickAddText = () => {
+    setAddText(text);
+    setText("");
+  }
+
   return (
     <div className="App">
-      Hello world!!
+      <input type = "text"value = {text}
+      onChange={(event) => setText(event.target.value)}/>
+      <button onClick={onClickAddText}>追加</button>
+      <p>リアルタイム：{text}</p>
+      <table>
+      <p>ボタンクリック：{addText}</p>
+      </table>
     </div>
   );
 }
